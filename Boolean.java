a74d7d5cc1403cbd4603027a53f6a35322d191a4
harshitbansal373 <harshitbansal373@gmail.com>
2019-02-20 18:59:56 +0530

    boolean data type

---
+++ Boolean.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Boolean
+{
+  public static void main(String args[]){
+	int a = 20;
+	int b = 30;
+	boolean c = a>b;
+	System.out.println("Result = "+c);
+  }
+}
+
+
+
+output
+---------------
+Result = false
